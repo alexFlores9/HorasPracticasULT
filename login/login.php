b05379e0cdda8c1d3ee3f55cc98cd312f0a48441
alexFlores9 <aleexflors123@gmail.com>
2020-12-16 11:37:11 -0600

    Create login.php
    Form login

---
+++ login/login.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="estLoging.css">
+</head>
+<body>
+
+    
+    
+        
+            
+        <form action="validar.php" method="POST">
+        <h2>Login</h2>
+  
+       <center> <label for="Usuario">Usuario</label></center>
+       
+        <input type="text" name="Usuario" placeholder="Usuario" required>
+        
+      
+        <center> <label for="Clave">Clave</label></center>
+        
+        <input type="password" name="Clave" placeholder="Clave" required>
+        
+        <input type="submit" value="Ingresar">
+        </form>
+    
+        
+        
+    
+</body>
+</html>
